2e7624f0ab4d0c390d31270d705a8ff8f30ea33d
nik popov <niK@iconluxurygroup.com>
2024-04-05 22:30:57 +0000

    fix pass issue 5??

--- settings.py
+++ settings.py
@@ -2,3 +2,8 @@ import os
 # Construct the path to the settings file
 BRANDSETTINGSPATH = os.path.join('task_settings/brand_settings.json')
 SERVERLESS_URL_SETTINGS="https://raw.githubusercontent.com/nikiconluxury/image-ip-mask-serverless/main/serverless_urls.json"
+HOST_V = 'db-mysql-nyc3-32053-do-user-15526822-0.c.db.ondigitalocean.com'
+DB_V =  'defaultdb'
+USER_V = 'doadmin'
+PASS_V = 'AVNS_DpjC8ACPq4bOcL3TuDl'
+PORT_V = 25060
