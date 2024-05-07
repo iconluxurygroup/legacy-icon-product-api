3e98ffc859c2148660ac9641975674e58c7bef95
Nik Popov <nik@iconluxurygroup.com>
2024-05-07 14:27:42 -0400

    adding celery config

--- celery_worker.py
+++ celery_worker.py
@@ -10,7 +10,11 @@ celery_app = Celery('celery_worker',
                     broker='amqp://guest:guest@rabbitmq:5672//',
                     result_backend='redis://redis:6379/0',
                     include=['tasks.celery_back_tasks','tasks.celery_back_tasks_image'])
-
+celery_app.conf.update(
+    task_acks_late=True,
+    task_reject_on_worker_lost=True,
+    #worker_state_db="./celery-state.db",
+)
 
 
 #'redis://redis:6379/0'#
